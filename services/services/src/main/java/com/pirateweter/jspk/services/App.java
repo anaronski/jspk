c134bff1adcba4e28d37450ac6fb342839ad5bf7
Andrei Naronski <andrei_naronski@epam.com>
2015-01-06 10:32:55 -0430

    first commit

---
+++ services/services/src/main/java/com/pirateweter/jspk/services/App.java
@@ -0,0 +1,13 @@
+package com.pirateweter.jspk.services;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
